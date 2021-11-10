9f963c76a12fa99e670b59b4895186e7392ac966
Vedant Roy <vroy101@gmail.com>
2021-11-09 23:31:58 -0500

    Update help message

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const HELP_MSG = `
-Usage: [binary_name] directory snippet_or_file <FLAGS> [--fileobj=<file>] [--ctx=<js_snippet>]
+Usage: [binary_name] directory snippet_or_file <FLAGS> [--fileObj=<file>] [--ctx=<js_snippet>]
 
 FLAGS:
--h, --help:      Show this message
--d, --delete:    Allow file deletion
--p, --preview:   Show a preview of the changes instead of executing them
+-h, --help       Show this message
+-d, --delete     Allow file deletion
+-p, --preview    Show a preview of the changes instead of executing them
 -r, --recursive  Recurse on all sub-directories
 -q, --quiet      Don't print unless there's an error
 -o, --overwrite  Allow overwriting files with the copy or move operations
